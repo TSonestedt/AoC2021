fd6139ba1593735147e2665e6e397a808e0c0120
Tilian Sonestedt <tilian.sonestedt@youcal.com>
2021-12-02 17:07:21 +0100

    början av day2

---
+++ AoC2021/Day 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_1
+{
+    class Program
+    {
+        static List<String[]> commands = new List<String[]>();
+        static void Main(string[] args)
+        {
+            foreach (string line in File.ReadLines(@"C:\Users\Tilian\source\repos\AoC2021\AoC2021\Day 2\CommandInputs.txt"))
+            {
+                commands.Add(line.Split(" "));
+
+            }
+            foreach (String[] line in commands)
+            {
+                var horizontalPos = 0;
+                var depth = 0;
+                var distance = Int32.Parse(line[1]);
+
+                switch (line[0])
+                {
+                    case "forward":
+                        horizontalPos += distance;
+                        break;
+                    case "down":
+                        depth += distance;
+                        break;
+                    case "up":
+                        depth -= distance;
+                        break;
+                }
+                    
+
+            }
+            return;
+        }
+    }
+
+}
