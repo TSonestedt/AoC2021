85ea3f9467bf04b8a981f80326159dcffd2e5e95
Tilian <tilian.sonestedt@youcal.com>
2021-12-04 20:07:29 +0100

    Day 4

---
+++ AoC2021/Day 3/Day 3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_1
+{
+    class Program
+    {
+        static List<string> diagnosticReport = new List<string>();
+        static void Main(string[] args)
+        {
+            foreach (var line in File.ReadLines(@$"{Directory.GetCurrentDirectory()}\DiagnosticReport.txt"))
+            {
+                diagnosticReport.Add(line);
+            }
+
+            FirstStar();
+            SecondStar();
+
+        }
+
+        static void FirstStar()
+        {
+            int[] numOfOnes = new int[12];
+            int[] numOfZeros = new int[12];
+            string gamma = "";
+            string epsilon = "";
+
+            foreach (string binaryNumber in diagnosticReport)
+            {
+                for (int index = 0; index < binaryNumber.Length; index++)
+                {
+                    if (binaryNumber[index] == '1')
+                    {
+                        numOfOnes[index]++;
+                    }
+                }
+            }
+
+            for (int index = 0; index < numOfOnes.Length; index++)
+            {
+                numOfZeros[index] = diagnosticReport.Count - numOfOnes[index];
+
+                if (numOfOnes[index] > numOfZeros[index])
+                {
+                    gamma += '1';
+                    epsilon += '0';
+
+                }
+                else
+                {
+                    gamma += '0';
+                    epsilon += '1';
+                }
+            }
+
+            var gammaRate = Convert.ToInt32(gamma, 2);
+            var epsilonRate = Convert.ToInt32(epsilon, 2);
+
+            Console.WriteLine(gammaRate * epsilonRate);
+            return;
+        }
+
+        static void SecondStar()
+        {
+            List<string> oxygenGeneratorRating = new List<string>();
+            List<string> co2ScrubberRating = new List<string>();
+
+            foreach (var number in diagnosticReport)
+            {
+                oxygenGeneratorRating.Add(number);
+                co2ScrubberRating.Add(number);
+            }
+
+            oxygenGeneratorRating = FindRating(oxygenGeneratorRating, 0, true);
+            co2ScrubberRating = FindRating(co2ScrubberRating, 0, false);
+
+            var oxygen = Convert.ToInt32(oxygenGeneratorRating.ElementAt(0), 2);
+            var co2 = Convert.ToInt32(co2ScrubberRating.ElementAt(0), 2);
+
+            Console.WriteLine(oxygen * co2);
+            return;
+        }
+
+        static List<string> FindRating(List<string> list, int index, bool oxygen)
+        {
+
+            if (list.Count > 1)
+            {
+                var numberOfOnes = 0;
+
+                numberOfOnes = list.Count(number => number[index] == '1');
+
+                if (numberOfOnes >= list.Count - numberOfOnes)
+                {
+                    list = list.Where(number =>
+                    {
+                        if (oxygen && number[index] != '1')
+                        {
+                            return false;
+                        }
+                        else if (!oxygen && number[index] == '1')
+                        {
+                            return false;
+                        }
+                        return true;
+
+                    }).ToList();
+                }
+                else
+                {
+                    list = list.Where(number =>
+                    {
+                        if (oxygen && number[index] != '0')
+                        {
+                            return false;
+                        }
+                        else if (!oxygen && number[index] == '0')
+                        {
+                            return false;
+                        }
+                        return true;
+
+                    }).ToList();
+                }
+
+                list = FindRating(list, ++index, oxygen);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ AoC2021/Day 4/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_4
+{
+    class Program
+    {
+        static List<int> numbers = new List<int>();
+        static List<int[,]> boards = new List<int[,]>();
+        static List<int[,]> boards_copy = new List<int[,]>();
+        static List<int[,]> markedBoards = new List<int[,]>();
+        static List<int[,]> markedBoards_copy = new List<int[,]>();
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText(@$"{Directory.GetCurrentDirectory()}\BingoData.txt");
+            string[] inputs = input.Split("\r\n\r\n");
+
+            string[] numbersFromFile = inputs[0].Split(",");
+
+            foreach (string number in numbersFromFile)
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+
+            for (int boardIndex = 1; boardIndex < inputs.Length; boardIndex++)
+            {
+                string[] rows = inputs[boardIndex].Split("\r\n");
+                int[,] board = new int[5, 5];
+                int[,] emptyBoard = new int[5, 5];
+                int[,] board_copy = new int[5, 5];
+                int[,] emptyBoard_copy = new int[5, 5];
+
+                for (int row = 0; row < rows.Length; row++)
+                {
+                    string[] numbersInRow = rows[row].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    for (int column = 0; column < numbersInRow.Length; column++)
+                    {
+                        board[row, column] = Convert.ToInt32(numbersInRow[column]);
+                        board_copy[row, column] = Convert.ToInt32(numbersInRow[column]);
+                        emptyBoard[row, column] = -1;
+                        emptyBoard_copy[row, column] = -1;
+                    }
+
+                }
+
+                boards.Add(board);
+                boards_copy.Add(board_copy);
+                markedBoards.Add(emptyBoard);
+                markedBoards_copy.Add(emptyBoard_copy);
+
+            }
+
+            FirstStar();
+            SecondStar();
+
+        }
+
+        static void FirstStar()
+        {
+            var score = 0;
+
+            foreach (int number in numbers)
+            {
+                foreach (var board in boards)
+                {
+
+                    for (int row = 0; row < board.GetLength(0); row++)
+                    {
+                        for (int column = 0; column < board.GetLength(1); column++)
+                        {
+                            if (board[row, column] == number)
+                            {
+                                board[row, column] = -1;
+
+                                if (CheckIfBingo(row, column, board))
+                                {
+                                    score = SumOfUnmarkedNumbers(board) * number;
+                                    Console.WriteLine(score);
+                                    return;
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        static bool CheckIfBingo(int row, int column, int[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (board[i, column] > -1)
+                {
+                    for (int j = 0; j < board.GetLength(1); j++)
+                    {
+                        if (board[row, j] > -1)
+                        {
+                            return false;
+                        }
+                    }
+
+                }
+
+            }
+            return true;
+        }
+
+        static int SumOfUnmarkedNumbers(int[,] board)
+        {
+            int sum = 0;
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] > -1)
+                    {
+                        sum += board[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+        static void SecondStar()
+        {
+            var score = 0;
+            var currentNumber = 0;
+            var counter = 0;
+            foreach (int number in numbers)
+            {
+                currentNumber = number;
+
+                for (int row = 0; row < 5; row++)
+                {
+                    for (int column = 0; column < 5; column++)
+                    {
+                        boards_copy = boards_copy.Where(board =>
+                        {
+                            if (board[row, column] == number)
+                            {
+                                board[row, column] = -1;
+                                if (CheckIfBingo(row, column, board))
+                                {
+                                    score = SumOfUnmarkedNumbers(board) * currentNumber;
+                                    counter++;
+                                    Console.WriteLine(@$"{counter}, {score}");
+                                    return false;
+                                }
+
+                            }
+                            return true;
+                        }).ToList();
+                    }
+                }
+            }
+        }
+
+    }
+}
