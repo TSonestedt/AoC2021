dc5194d5094acfa3362ccc4898e094164b74b4d6
Tilian <tilian.sonestedt@youcal.com>
2021-12-15 20:28:09 +0100

    Day 14

--- AoC2021/Day 14/Program.cs
+++ AoC2021/Day 14/Program.cs
@@ -16,18 +16,18 @@ namespace Day_14
 
         }
 
-        static (string polymerTemplate, Dictionary<string, char> pairInsertions) ReadInput()
+        static (string polymerTemplate, Dictionary<string, (char charToInsert, long occurances)> pairInsertions) ReadInput()
         {
             var polymerizationData = File.ReadAllText(@$"{Directory.GetCurrentDirectory()}\PolymerTemplate.txt");
 
             var polymerTemplate = polymerizationData.Split("\r\n\r\n")[0];
             var instructions = polymerizationData.Split("\r\n\r\n")[1].Split("\r\n").ToList();
 
-            var pairInsertions = new Dictionary<string, char>();
+            var pairInsertions = new Dictionary<string, (char charToInsert, long occurances)>();
 
             foreach (var instruction in instructions)
             {
-                pairInsertions.Add(instruction.Split(" -> ")[0], Convert.ToChar(instruction.Split(" -> ")[1]));
+                pairInsertions.Add(instruction.Split(" -> ")[0], (Convert.ToChar(instruction.Split(" -> ")[1]), 0));
             }
 
             return (polymerTemplate, pairInsertions);
@@ -35,30 +35,11 @@ namespace Day_14
         }
         static void FirstStar()
         {
-            var (polymerTermplate, pairInsertions) = ReadInput();
-            string polymerizedTemplate = polymerTermplate;
+            var (polymerTemplate, pairInsertions) = ReadInput();
+            var polymerizedTemplate = pairInsertions;
 
-            for (int step = 0; step < 10; step++)
-            {
-                polymerizedTemplate = InsertChar(polymerizedTemplate, pairInsertions);
-            }
-
-            var occuringChars = new Dictionary<char, int>();
-            occuringChars = CountCharsOccurances(polymerizedTemplate, occuringChars);
-
-            var mostCommonCharOccurances = occuringChars.Select(c => c.Value).Max();
-            var leastCommonCharOccurances = occuringChars.Select(c => c.Value).Min();
-
-            Console.WriteLine(mostCommonCharOccurances - leastCommonCharOccurances);
-
-            return;
-
-        }
-
-        static string InsertChar(string polymerTemplate, Dictionary<string, char> pairInsertions)
-        {
             var polymerTemplatePairs = new List<string>();
-            var charsToInsert = new List<char>();
+
 
             for (int p = 0; p < polymerTemplate.Length - 1; p++)
             {
@@ -67,33 +48,61 @@ namespace Day_14
 
             foreach (var pair in polymerTemplatePairs)
             {
-                charsToInsert.Add(pairInsertions[pair]);
+                pairInsertions[pair] = (pairInsertions[pair].charToInsert, +1);
             }
 
-            var indexToInsertAt = 1;
-
-            for (int i = 0; i < charsToInsert.Count; i++)
+            for(int step = 0; step < 10; step++)
             {
-                polymerTemplate = polymerTemplate.Insert(indexToInsertAt, Convert.ToString(charsToInsert[i]));
-                indexToInsertAt += 2;
+                polymerizedTemplate = InsertChars(polymerizedTemplate);
             }
 
+            var occuringChars = new Dictionary<char, long>();
+            occuringChars = CountCharsOccurances(polymerizedTemplate, occuringChars);
 
-            return polymerTemplate;
+            var lastChar = polymerTemplate[polymerTemplate.Length - 1];
+            occuringChars[lastChar]++;
+
+            var mostCommonCharOccurances = occuringChars.Select(c => c.Value).Max();
+            var leastCommonCharOccurances = occuringChars.Select(c => c.Value).Where(c => c > 0).Min();
+
+            Console.WriteLine(mostCommonCharOccurances - leastCommonCharOccurances);
+
+            return;
+
         }
 
-        static Dictionary<char, int> CountCharsOccurances(string polymerizedTemplate, Dictionary<char, int> occuringChars)
+        static Dictionary<string, (char charToinsert, long occurances)> InsertChars(Dictionary<string, (char charToinsert, long occurances)> pairInsertions)
         {
-            for (int i = 0; i < polymerizedTemplate.Length; i++)
+            var insertedPairs = new Dictionary<string, (char charToinsert, long occurances)>();
+
+            foreach (var pair in pairInsertions)
             {
-                if (occuringChars.ContainsKey(polymerizedTemplate[i]))
+                insertedPairs.Add(pair.Key, (pair.Value.charToinsert, 0));
+            }
+            foreach (var pair in pairInsertions)
+            {
+                if (pair.Value.occurances > 0)
                 {
-                    occuringChars[polymerizedTemplate[i]]++;
+                    var newPair1 = $"{pair.Key[0]}{pair.Value.charToinsert}";
+                    var newPair2 = $"{pair.Value.charToinsert}{pair.Key[1]}";
+                    insertedPairs[newPair1] = (insertedPairs[newPair1].charToinsert, insertedPairs[newPair1].occurances + pair.Value.occurances);
+                    insertedPairs[newPair2] = (insertedPairs[newPair2].charToinsert, insertedPairs[newPair2].occurances + pair.Value.occurances);
                 }
-                else
+            }
+
+
+            return insertedPairs;
+        }
+
+        static Dictionary<char, long> CountCharsOccurances(Dictionary<string, (char charToinsert, long occurances)> polymerizedTemplate, Dictionary<char, long> occuringChars)
+        {
+            foreach (var pair in polymerizedTemplate)
+            {
+                if(!occuringChars.TryAdd(pair.Key[0], pair.Value.occurances))
                 {
-                    occuringChars.Add(polymerizedTemplate[i], 1);
+                    occuringChars[pair.Key[0]] += pair.Value.occurances;
                 }
+              
             }
 
 
@@ -102,7 +111,39 @@ namespace Day_14
 
         static void SecondStar()
         {
+            var (polymerTemplate, pairInsertions) = ReadInput();
+            var polymerizedTemplate = pairInsertions;
 
+            var polymerTemplatePairs = new List<string>();
+
+
+            for (int p = 0; p < polymerTemplate.Length - 1; p++)
+            {
+                polymerTemplatePairs.Add(polymerTemplate.Substring(p, 2));
+            }
+
+            foreach (var pair in polymerTemplatePairs)
+            {
+                pairInsertions[pair] = (pairInsertions[pair].charToInsert, +1);
+            }
+
+            for (int step = 0; step < 40; step++)
+            {
+                polymerizedTemplate = InsertChars(polymerizedTemplate);
+            }
+
+            var occuringChars = new Dictionary<char, long>();
+            occuringChars = CountCharsOccurances(polymerizedTemplate, occuringChars);
+
+            var lastChar = polymerTemplate[polymerTemplate.Length - 1];
+            occuringChars[lastChar]++;
+
+            var mostCommonCharOccurances = occuringChars.Select(c => c.Value).Max();
+            var leastCommonCharOccurances = occuringChars.Select(c => c.Value).Where(c => c > 0).Min();
+
+            Console.WriteLine(mostCommonCharOccurances - leastCommonCharOccurances);
+
+            return;
         }
 
     }
